bc097959657fd8c2d62642c4a2ac270dfac0311c
balu <balu@mobile>
2021-06-26 17:37:26 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # covid-vaccination-cert_experiments
 some experiments with the official covid vaccination certificates
+
+
+## interesting Links
+
+- https://github.com/corona-warn-app/cwa-app-ios/blob/46c34c27d891663843b201fd9d1ec87c3d60cfdd/src/xcode/ENA/HealthCertificateToolkit/Sources/HealthCertificateToolkit/DigitalGreenCertificateAccess.swift
+- https://github.com/corona-warn-app/cwa-app-ios/blob/12d17e48d4ac894f06ce12419a6dbe6dfea3f289/src/xcode/ENA/ENA/Source/Services/HealthCertificate/HealthCertificateService.swift
+- https://w3c-ccg.github.io/vaccination-vocab/
+- https://dgc.a-sit.at/ehn/
+- https://pypi.org/project/cbor2/
---
+++ decode.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+from base45 import b45decode, b45encode
+import zlib
+import cbor2
+
+import argparse
+
+
+def extractCborData(base45:str) -> bytes:
+	#check for prefix "HC1:"
+	if cert[:4] != "HC1:":
+		print("Error - Prefix missmatch")
+		exit()
+
+	cert_without_prefix = cert[4:]
+
+	zip_data = b45decode(cert_without_prefix)
+
+	cborData = zlib.decompress(zip_data)
+
+	return cborData
+
+def extractCborHeader(cborData:bytes):
+	#print(cbor2.loads(cborData))
+	webTokenResult = decodeCBORWebTokenPayload(cborData)
+	#webTokenResult
+	#// 1: Issuer (2-letter country code)
+	# 4: Expiration time (UNIX timestamp in seconds)
+	#6: Issued at (UNIX timestamp in seconds)
+	return { 'issuer': webTokenResult[1], 'expiration_time': webTokenResult[4], 'issued_at': webTokenResult[6] }
+
+
+
+def decodeCBORWebTokenPayload(cborData:bytes):
+	cborValue = decodeCBORWebTokenEntries(cborData)
+	sub_payload = cbor2.loads(cborValue[2])
+	return sub_payload
+
+
+def decodeCBORWebTokenEntries(cborData:bytes):
+	cborPayload = cbor2.loads(cborData)
+	# 18: CBOR tag value for a COSE Single Signer Data Object
+	if not isinstance(cborPayload, cbor2.CBORTag):
+		raise Error("Type error")
+	if cborPayload.tag != 18:
+		raise Error("Type error")
+
+	cborValue = cborPayload.value
+
+	#The message has to have 4 entries.
+	if not len(cborValue) == 4:
+		raise Error("Type error")
+
+	return cborValue
+
+def extractCertificate(cborData:bytes):
+	cborPayload = decodeCBORWebTokenPayload(cborData)
+	#extractDigitalGreenCertificate
+	#// -260: Container of Digital Green Certificate
+	healthCertificateElement = cborPayload[-260]
+	#// 1: Digital Green Certificate
+	healthCertificateCBOR = healthCertificateElement[1]
+	#todo 
+	#validateSchema
+	
+
+	#print(healthCertificateCBOR)
+	return healthCertificateCBOR
+
+
+if __name__ == "__main__":
+	# create parser
+	parser = argparse.ArgumentParser(description="Decode a vaccination cert")
+
+	# add arguments to the parser
+	parser.add_argument("certAsString", help='Certificat in form of a String, it should start with HC1')
+	# parse the arguments
+	args = parser.parse_args()
+	cert = args.certAsString
+
+
+	cborData = extractCborData(cert)
+	print("Alles:")
+	print(decodeCBORWebTokenPayload(cborData))
+	print("Header:")
+	print(extractCborHeader(cborData))
+	print("Cert:")
+	print(extractCertificate(cborData))
+
+
